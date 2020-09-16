47ed6bcb7c49adc58bb4a5ac4f48293efe47def5
Demetrio Lima <dlima.nyit@gmail.com>
2020-09-16 21:47:28 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -13,7 +13,6 @@ end
 
 def using_pop(continents)
   continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
-  return continents.pop
-  return continents.size
+  return continents.pop + continents.size
 end
 
